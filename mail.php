f5ff1fb35d31ca49845953ab110b8d4443cc35c3
Ibooking <fabio@ibooking.com.br>
2021-07-19 21:38:57 -0300

    Envio de Email pelo Formulário de Contato.

--- mail.php
+++ mail.php
@@ -1,11 +1,18 @@
 <?php
 
+ini_set( 'display_errors', 1 );
 ini_set('SMTP','smtp-mail.outlook.com');
 ini_set('smtp_port', 587);
+ini_set('smtp_ssl', 'auto');
 
 $to_email = $_POST['email'];
 $subject  = $_POST['subject'];
 $message  = $_POST['message'];
-$headers  = 'From: fabio.santos@s4sys.com.br';
+
+$headers = [
+    'From'     => 'fabio.santos@s4sys.com.br',
+    'Reply-To' => 'fabio.santos@s4sys.com.br',
+    'X-Mailer' => 'PHP/' . phpversion()
+];
 
 mail($to_email,$subject,$message,$headers);
