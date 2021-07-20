8a5262daa70db74de8a8985bca12390265f85442
Ibooking <fabio@ibooking.com.br>
2021-07-19 22:34:24 -0300

    Envio do Email de Contato pela Classe PhpMailer.

--- mail.php
+++ mail.php
@@ -1,23 +1,25 @@
 <?php
-/*
-ini_set( 'display_errors', 1 );
-ini_set('SMTP','smtp-mail.outlook.com');
-ini_set('smtp_port', 587);
-ini_set('smtp_ssl', 'auto');
-*/
-$to_email = $_POST['email'];
-$subject  = $_POST['subject'];
-$message  = $_POST['message'];
 
-$headers = [
-    'From'     => 'fabio.santos@s4sys.com.br',
-    'Reply-To' => 'fabio.santos@s4sys.com.br',
-    'X-Mailer' => 'PHP/' . phpversion()
-];
+require_once 'app/PhpMailer/PhpMailer.php';
 
-try{
-    mail($to_email,$subject,$message,$headers);
-} catch(\Throwable $exception){
-    echo $exception->getMessage();
-}
+$mailto  = $_POST['email'];
+$mailSub = $_POST['subject'];
+$mailMsg = $_POST['message'];
 
+$mail = new PHPMailer();
+$mail->IsSmtp();
+$mail->SMTPDebug = 2;
+
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'tls';
+$mail->Host = "smtp-mail.outlook.com";
+$mail->Port = 587;
+
+$mail->IsHTML(true);
+$mail->Username = "fabio.santos@s4sys.com.br";
+$mail->Password = "uska#galo2021";
+$mail->setFrom("fabio.santos@s4sys.com.br");
+$mail->Subject = $mailSub;
+$mail->Body = $mailMsg;
+$mail->AddAddress($mailto);
+$mail->Send();
