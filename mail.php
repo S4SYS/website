5d3a68e65c78004337548893632f852221b53eac
Ibooking <fabio@ibooking.com.br>
2021-07-21 10:30:36 -0300

    Melhoria na mensagem do Email de Contato.

--- mail.php
+++ mail.php
@@ -2,27 +2,73 @@
 
 require_once 'app/PhpMailer/PhpMailer.php';
 
-$mailto  = $_POST['email'];
-$mailSub = $_POST['subject'];
-$mailMsg = $_POST['message'];
 
-$mail = new PHPMailer();
-$mail->IsSmtp();
-$mail->SMTPDebug = 2;
+final Class EmailContato
+{
+    const SMTP_HOST   = 'smtp-mail.outlook.com';
+    const SMTP_PORT   = 587;
+    const SMTP_AUTH   = true;
+    const SMTP_SECURE = 'tls';
+    const SMTP_DEBUG  = 2;
+    
+    const MAIL_USER = 'fabio.santos@s4sys.com.br'; // TODO: mudar para contato @s4sys.com.br
+    const MAIL_PASS = 'uska#galo2021';
+    const MAIL_FROM = 'fabio.santos@s4sys.com.br'; // TODO: mudar para contato @s4sys.com.br
 
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = 'tls';
-$mail->Host = "smtp-mail.outlook.com";
-$mail->Port = 587;
+    private $nome;
+    private $email;
+    private $assunto;
+    private $textMessage;
+    private $mailer;
 
-$mail->IsHTML(true);
-$mail->Username = "fabio.santos@s4sys.com.br";
-$mail->Password = "uska#galo2021";
-$mail->setFrom("fabio.santos@s4sys.com.br");
-$mail->Subject = $mailSub;
-$mail->Body = $mailMsg;
-$mail->AddAddress($mailto);
-$mail->Send();
+    /**
+     * @param array $postData
+     */
+    public function __construct($postData)
+    {
+        $this->nome        = $postData['name'];
+        $this->email       = $postData['email'];
+        $this->assunto     = $postData['subject'];
+        $this->textMessage = $postData['message'];
+    }
 
-//@header('Location: index.php');
-echo "<script>location.href='index.php';</script>";
+    /**
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->mailer = new PHPMailer();
+        $this->mailer->IsSmtp();
+        $this->mailer->SMTPDebug = self::SMTP_DEBUG;
+
+        $this->mailer->SMTPAuth = self::SMTP_AUTH;
+        $this->mailer->SMTPSecure = self::SMTP_SECURE;
+        $this->mailer->Host = self::SMTP_HOST;
+        $this->mailer->Port = self::SMTP_PORT;
+
+        $this->mailer->IsHTML(true);
+        $this->mailer->Username = self::MAIL_USER;
+        $this->mailer->Password = self::MAIL_PASS;
+        $this->mailer->setFrom(self::MAIL_FROM);
+        $this->mailer->Subject = $this->assunto;
+        $this->mailer->Body = $this->getEmailMessage();
+        $this->mailer->AddAddress($this->email);
+        $this->mailer->Send();
+    }
+
+    /**
+     * @return string
+     */
+    private function getEmailMessage(): string
+    {
+        return implode('', [
+            "<ul>",
+            "<li>Nome: {$this->nome}</li>",
+            "<li>Email: {$this->email}</li>",
+            "<li>Mensagem: {$this->textMessage}</li>",
+            "</ul>"
+        ]);
+    }
+}
+
+(new EmailContato($_POST))->init();
