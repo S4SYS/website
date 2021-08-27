6c487681909f2fce947495121c45de88abc2a7c2
Hudson Falcão <hudson.falcao@infobase.com.br>
2021-08-27 12:08:09 -0300

    Inserção da HIstória da S4SYS

--- views/historia.php
+++ views/historia.php
@@ -42,5 +42,21 @@
 
     </div>
     <!--/.container-->
+
+
+</section>
+<section class="section">
+     <div class="row">
+            <div class="text-center col-md-6 col-lg-offset-3">
+                <h2 class="title">História da S4Sys</h2>
+                <p>Surgimos da Digital Datacenter, uma empresa voltada para plataformas sustentadas por tecnologias web e evoluímos nos últimos anos para a concepção de Sistemas de Informação, observando a evolução constante das áreas de negócios, suas demandas e anseios.</p>
+                <p>Um marco importante nesse processo, foi quando os Executivos do Grupo 4 Seniors decidiram direcionar a área de Desenvolvimento de Sistemas da 4 Seniors Brasil, que atuava com foco específico em softwares especialistas, em abrir e potencializar o Portfolio, interligando a camada de negócios com a tecnologia com o objetivo principal em manter-se como referêncial qualitativo no atendimento de clientes e parceiros de negócio, garantindo atuação, com alto valor agregado, em todo território nacional e países estratégicos.</p>
+                <p>Com a expertise de mais de uma década da Digital Datacenter, notória atuação e sólidos conhecimentos de mais de duas décadas da 4 Seniors Brasil, nasce a S4SYS - Smart 4 System, uma Startup conectada com metodologias ágeis e tecnologias de vanguarda, com um novo quadro societário, possibilitando convergência de habilidades multidisciplinares, enfrentando já neste início grandes desafios e promovendo soluções com objetivos de destaques e diferenciais únicos no segmentos de Sistemas de Informação.</p>
+                <p>Atualmente a empresa se estrutura com projetos de visibilidade e já conta com iniciativas de um Centro de Pesquisa, Desenvolvimento e Inovação para criação de Produtos.</p>
+                <p>A S4SYS conta com uma equipe experiente, implementando soluções confiáveis, de alto valor para as organizações, pois nos preocupamos com a qualidade e satisfação dos nossos clientes.</p>
+                <p>Em 2021, a S4SYS inaugura sua sede na Cidade de Niterói/RJ, e já amplia sua capacidade colaborativa expandindo o Time de especialistas para melhor atender seus clientes e objetivos.</p>
+            </div>
+     </div>
+
 </section>
 <!--End History-->
