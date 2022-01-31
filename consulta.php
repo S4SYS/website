c28cd42001089a91253bca9ee077ee37be1ec086
segredo123 <fabio@ibooking.com.br>
2022-01-31 12:14:01 -0300

    Topo com logo sem menu na página de consulta.

--- consulta.php
+++ consulta.php
@@ -4,31 +4,50 @@ if(!isset($_POST['codigo'])) header('Location: ./');
 
 require_once 'app/ApiRequest.php';
 
-final class Consulta { use ApiRequest; }
+final class Consulta
+{
+    use ApiRequest;
+}
 
 ?>
 <html lang="pt-br" class="no-js">
 
 <head>
-  <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-  <meta charset="utf-8">
-  <title>S4Sys - Smart 4 System</title>
-  <meta name="author" content="">
-  <meta name="keywords" content="s4sys">
-  <meta name="description" content="Inteligência para Negócios Inteligentes">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    <meta charset="utf-8">
+    <title>S4Sys - Smart 4 System</title>
+    <meta name="author" content="">
+    <meta name="keywords" content="s4sys">
+    <meta name="description" content="Inteligência para Negócios Inteligentes">
 
-  <!-- Mobile Specific Metas -->
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- Mobile Specific Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!--styles -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <link href="js/owl-carousel/owl.carousel.css" rel="stylesheet">
+    <link href="js/owl-carousel/owl.theme.css" rel="stylesheet">
+    <link href="js/owl-carousel/owl.transitions.css" rel="stylesheet">
+    <link href="css/magnific-popup.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/animate.css" />
+    <link rel="stylesheet" href="css/etlinefont.css">
+    <link href="css/style.css" type="text/css" rel="stylesheet" />
 
 
-  <!--styles -->
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/font-awesome.css" rel="stylesheet">
-  <link rel="stylesheet" href="css/etlinefont.css">
-  <link href="css/style.css" type="text/css" rel="stylesheet" />
 </head>
-<body>
-    <section>
+
+<body data-target="#main-menu">
+    <!--Start Page loader -->
+    <div id="pageloader">
+        <div class="loader text-center">
+            <img src="images/progress.gif" alt='loader' />
+        </div>
+    </div>
+    <!--End Page loader -->
+    <?php require_once 'views/topo_sem_menu.php'; ?>
+    
+    <section class="section">
         <div class="container">
             <div class="row">
                 <div class="title-box text-center">
@@ -43,62 +62,71 @@ final class Consulta { use ApiRequest; }
                     <table class="table table-bordered .table-condensed" id="tableConsulta">
                         <thead></thead>
                         <tbody></tbody>
-                    </table> 
+                    </table>
                 </div>
             </div>
         </div>
     </section>
+    <!--Plugins-->
+    <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/owl-carousel/owl.carousel.js"></script>
+    <script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+    <script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+    <script type="text/javascript" src="js/easing.js"></script>
+    <script type="text/javascript" src="js/jquery.easypiechart.js"></script>
+    <script type="text/javascript" src="js/jquery.appear.js"></script>
+    <script type="text/javascript" src="js/jquery.parallax-1.1.3.js"></script>
+    <script type="text/javascript" src="js/jquery.mixitup.min.js"></script>
+    <script type="text/javascript" src="js/custom.js"></script>
+
 </body>
+
 </html>
 
 <script>
-    class ViewConsulta
-    {
+    class ViewConsulta {
         dadosApi;
         dadosTabela;
         thead;
         tbody;
-      
-        init()
-        {
-            document.getElementById('backConsulta').addEventListener('click', ()=>{ location.href = './'});
-            this.dadosApi = JSON.parse('<?=(new Consulta())->get("consulta&codigo={$_POST['codigo']}");?>');
+
+        init() {
+            document.getElementById('backConsulta').addEventListener('click', () => {
+                location.href = './'
+            });
+            this.dadosApi = JSON.parse('<?= (new Consulta())->get("consulta&codigo={$_POST['codigo']}"); ?>');
             console.log(JSON.stringify(this.dadosApi));
             this.identificarDados();
             this.setTable();
         }
 
-        identificarDados()
-        {
-            if(!this.dadosApi.violacao.data)
+        identificarDados() {
+            if (!this.dadosApi.violacao.data)
                 this.setDadosRequisicao();
-            else 
-                this.setDadosViolacao();            
+            else
+                this.setDadosViolacao();
         }
 
-        setDadosRequisicao()
-        {
+        setDadosRequisicao() {
             this.dadosTabela = this.dadosApi.requisicao.data;
             this.thead = this.getTableHeadRequisicao();
             this.tbody = this.getTableBodyRequisicao();
         }
 
-        setDadosViolacao()
-        {
+        setDadosViolacao() {
             this.dadosTabela = this.dadosApi.violacao.data;
             this.thead = this.getTableHeadViolacao();
             this.tbody = this.getTableBodyViolacao();
         }
 
-        setTable()
-        {
-            let table = document.getElementById('tableConsulta'); 
+        setTable() {
+            let table = document.getElementById('tableConsulta');
             table.children[0].innerHTML = this.thead;
-            table.children[1].innerHTML = this.tbody;            
+            table.children[1].innerHTML = this.tbody;
         }
 
-        getTableHeadViolacao()
-        { 
+        getTableHeadViolacao() {
             return [
                 '<tr>',
                 '<th>C&oacute;digo</th>',
@@ -113,14 +141,13 @@ final class Consulta { use ApiRequest; }
             ].join('');
         }
 
-        getTableHeadRequisicao()
-        { 
+        getTableHeadRequisicao() {
             return [
                 '<tr>',
                 '<th>C&oacute;digo</th>',
                 '<th>CPF</th>',
                 '<th>Email</th>',
-                '<th>Telefone</th>',                
+                '<th>Telefone</th>',
                 '<th>Tipo de Requisi&ccedil;&atilde;o</th>',
                 '<th>Pedido</th>',
                 '<th>Setor</th>',
@@ -131,8 +158,7 @@ final class Consulta { use ApiRequest; }
             ].join('');
         }
 
-        getTableBodyViolacao()
-        {
+        getTableBodyViolacao() {
             return [
                 '<tr>',
                 `<td>${this.dadosTabela.codigo}</td>`,
@@ -147,14 +173,13 @@ final class Consulta { use ApiRequest; }
             ].join('');
         }
 
-        getTableBodyRequisicao()
-        {
+        getTableBodyRequisicao() {
             return [
                 '<tr>',
                 `<td>${this.dadosTabela.codigo}</td>`,
                 `<td>${this.dadosTabela.cpf}</td>`,
                 `<td>${this.dadosTabela.email}</td>`,
-                `<td>${this.dadosTabela.telefone}</td>`,                
+                `<td>${this.dadosTabela.telefone}</td>`,
                 `<td>${this.dadosTabela.nome_tipo_requisicao}</td>`,
                 `<td>${this.dadosTabela.pedido}</td>`,
                 `<td>${this.dadosTabela.nome_setor}</td>`,
---
+++ views/topo_sem_menu.php
@@ -0,0 +1,35 @@
+<!--Start Navigation-->
+<header id="header" style="margin-top:1em;">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="fa fa-bars"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!--Start Logo -->
+                <div class="logo-nav">
+                    <a href="index.php">
+                        <img src="images/logo_s4sys.png" alt="Company logo" />
+                    </a>
+                </div>
+                <!--End Logo -->
+
+                <div class="clear-toggle"></div>
+                <div id="main-menu" class="collapse scroll navbar-right">
+                    <ul class="nav">
+                        <li>
+                            <a target="_blank" href="https://www.facebook.com/S4SysBR"><i class="fa fa-facebook"></i></a>
+                            <a target="_blank" href="https://www.linkedin.com/company/s4sys/"><i class="fa fa-linkedin"></i></a>
+                            <a target="_blank" href="https://www.instagram.com/s4sys/"><i class="fa fa-instagram"></i></a>
+                            <a target="_blank" href="https://www.twitter.com/s4sys/"><i class="fa fa-twitter"></i></a>
+                        </li>
+                    </ul>
+                </div><!-- end main-menu -->
+            </div>
+        </div>
+    </div>
+</header>
+<!--End Navigation-->
