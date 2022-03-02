562f2a609e6a063910a09b047da1589a679211f5
segredo123 <fabio@ibooking.com.br>
2022-03-01 21:43:58 -0300

    Carrega conteúdo de cada página pela hash url.

--- adm/index.php
+++ adm/index.php
@@ -53,98 +53,23 @@ if(!isset($_SESSION['idUsuario'])) die("<script>location.href = 'login.php';</sc
             <!-- Divider -->
             <hr class="sidebar-divider my-0">
 
-            <!-- Nav Item - Dashboard -->
+            <!-- Nav Item - Requisicao -->
             <li class="nav-item">
-                <a class="nav-link" href="index.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
+                <a class="nav-link" data-hash="#requisicao">
+                    <i class="fas fa-fw fa-bullhorn"></i>
+                    <span>Requisi&ccedil;&atilde;o</span>
+                </a>
             </li>
 
             <!-- Divider -->
             <hr class="sidebar-divider">
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Interface
-            </div>
-
-            <!-- Nav Item - Pages Collapse Menu -->
+            <!-- Nav Item - Violacao -->
             <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
+                <a class="nav-link" data-hash="#violacao">
+                    <i class="fas fa-fw fa-ban"></i>
+                    <span>Viola&ccedil;&atilde;o</span>
                 </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
-                    </div>
-                </div>
-            </li>
-
-            <!-- Nav Item - Utilities Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-                    aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-fw fa-wrench"></i>
-                    <span>Utilities</span>
-                </a>
-                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                    data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="utilities-color.html">Colors</a>
-                        <a class="collapse-item" href="utilities-border.html">Borders</a>
-                        <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                        <a class="collapse-item" href="utilities-other.html">Other</a>
-                    </div>
-                </div>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Addons
-            </div>
-
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item active">
-                <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
-                    aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>Pages</span>
-                </a>
-                <div id="collapsePages" class="collapse show" aria-labelledby="headingPages"
-                    data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Login Screens:</h6>
-                        <a class="collapse-item" href="login.html">Login</a>
-                        <a class="collapse-item" href="register.html">Register</a>
-                        <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                        <div class="collapse-divider"></div>
-                        <h6 class="collapse-header">Other Pages:</h6>
-                        <a class="collapse-item" href="404.html">404 Page</a>
-                        <a class="collapse-item active" href="blank.html">Blank Page</a>
-                    </div>
-                </div>
-            </li>
-
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="charts.html">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
-            </li>
-
-            <!-- Nav Item - Tables -->
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
             </li>
 
             <!-- Divider -->
@@ -372,10 +297,12 @@ if(!isset($_SESSION['idUsuario'])) die("<script>location.href = 'login.php';</sc
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
-                <div class="container-fluid">
+                <div id="main" class="container-fluid">
 
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+                    <h1 id="pageTitle" class="h3 mb-4 text-gray-800"></h1>
+
+                    <span id="loader" class="text-center"></span>
 
                 </div>
                 <!-- /.container-fluid -->
@@ -433,6 +360,66 @@ if(!isset($_SESSION['idUsuario'])) die("<script>location.href = 'login.php';</sc
 
     <!-- Custom scripts for all pages-->
     <script src="js/sb-admin-2.min.js"></script>
+    
+    <!-- Page Views -->
+    <script>
+        const HASH_REQUISICAO = '#requisicao';
+        const HASH_VIOLACAO   = '#violacao' ;
+
+        /*
+        * Classe que trata o carregamento do conteudo interno, ao carregar a pagina e 
+        * de acordo com o menu selecionado.
+        */
+        class ViewFactory
+        {
+            $main;
+            $title;
+
+            constructor()
+            {
+                this.$main  = $('#main');
+                this.$title = this.$main.find('#pageTitle');
+            }
+
+            init()
+            {                
+                this.setLoader();
+                this.setContentByHash();                
+            }
+
+            setContentByHash()
+            {
+                switch(window.location.hash){
+                    case(HASH_REQUISICAO):
+                        this.$title.html('Requisi&ccedil;&otilde;es'); 
+                        break; 
+                    case(HASH_VIOLACAO):
+                        this.$title.html('Viola&ccedil;&otilde;es');   
+                        break;
+                    default: 
+                        this.$title.html('Requisi&ccedil;&otilde;es'); 
+                        break;
+                }
+            }
+
+            setLoader()
+            {
+                let $loader = this.$main.find('#loader'); 
+                $loader.html('<img src="img/loading.gif" width="124" height="124"">');
+                $loader.delay(1000).fadeOut("slow");
+            }
+        }
+        
+        let viewFactory = new ViewFactory(); 
+        viewFactory.init();
+
+        $('.nav-link').click(function(){
+            window.location.hash = this.dataset.hash;
+            let viewFactory = new ViewFactory();
+            viewFactory.init();
+        });
+    </script>
+
 
 </body>
 
